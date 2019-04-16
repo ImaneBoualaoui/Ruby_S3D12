9cb39fbab79b16e58962420891dc536a44dd2ddd
ImaneBoualaoui <imane.92@live.fr>
2019-04-16 15:36:29 +0200

    ajout de la class HumanPlayer

--- lib/player.rb
+++ lib/player.rb
@@ -10,7 +10,7 @@ class Player
 
 	#Méthode qui afficha l'état d'un joueur
 	def show_state
-		puts @name + " a #{life_points} points de vie"
+		puts @name + " a #{@life_points} points de vie"
 	end
 
 	#Méthode : subir une attaque
@@ -39,6 +39,64 @@ class Player
 
 		#on fait subir les dégâts à l'autre joueur avec en paramètre la valeur val
 		player2.gets_damage(val)
+	end
+end
 
+class HumanPlayer < Player
+	attr_accessor :weapon_level
+
+	def initialize(name)
+		@name = name
+		@life_points = 100
+		@weapon_level = 1
+	end
+
+	def show_state
+		puts @name +" a #{@life_points} points et une arme de niveau #{@weapon_level}"
+	end
+
+	def compute_damage
+		rand(1..6) * @weapon_level
+	end
+
+	def search_weapon
+		#Lancé de dé qui sera égal au niveau de la nouvelle arme trouvé
+		dice = rand(1..6)
+		puts "Tu as trouvé une arme de niveau #{dice}"
+
+		#On cherche à savoir si l'arme vaut le coup ou pas
+		if dice > @weapon_level
+			@weapon_level = dice
+			puts "Youhou ! elle est meilleure que ton arme actuelle : tu la prends."
+		else
+			puts "M@*#$... elle n'est pas mieux que ton arme actuelle..."
+		end
+	end
+
+	def search_health_pack
+		#on lance le dé
+		dice = rand(1..6)
+
+		if dice == 1
+			puts "Tu n'as rien trouvé"
+		elsif dice == 2 || dice == 3 || dice == 4 || dice == 5
+			#si sa vie est inférieur ou égal à 50 on sait que sa vie ne dépassera pas 100 points
+			if @life_points <= 50 
+				@life_points += 50
+			#Sinon on augmente sa vie en faisant attention de ne pas dépasser 100 points
+			else 
+				life = 100 - @life_points
+				@life_points += life # donc on ne peut pas lui augmenter de 50 points mais uniquement des points qui lui manque pour avoir 100 points
+			end
+			puts "Bravo, tu as trouvé un pack de +50 points de vie !"
+		else #même test qu'au dessus
+			if @life_points <= 20 
+				@life_points += 80
+			else 
+				life = 100 - @life_points
+				@life_points += life
+			end
+			puts "Waow, tu trouvé un pack de +80 points de vie !"
+		end			
 	end
 end
